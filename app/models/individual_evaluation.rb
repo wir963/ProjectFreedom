f63202602e5c55f9037602cd72fe79b419f7d380
Tyler Sax <tylersax@gmail.com>
2012-06-10 18:43:43 -0400

    added basic set of data validation methods

--- app/models/individual_evaluation.rb
+++ app/models/individual_evaluation.rb
@@ -4,4 +4,16 @@ class IndividualEvaluation < ActiveRecord::Base
   #belongs_to :professor, :through => :course_evaluation
   #belongs_to :course, :through => :course_evaluation
   
+    validates_presence_of :user
+    
+    validates_presence_of :course_id
+    validates_numericality_of :course_id
+    
+    validates_presence_of :professor_id
+    validates_numericality_of :professor_id
+    
+    validates_presence_of :rating
+    validates_numericality_of :rating, :greater_than_or_equal_to=>0, :less_than_or_equal_to=>0
+    
+  
 end
--- app/models/professor.rb
+++ app/models/professor.rb
@@ -3,4 +3,17 @@ class Professor < ActiveRecord::Base
     has_many :course_evaluations
     has_many :courses, :through => :course_evaluations
     has_many :individual_evaluations, :through => :course_evaluations
+
+    validates_presence_of :first_name
+    validates_length_of :first_name, :maximum=>140 
+    
+    validates_presence_of :last_name
+    validates_length_of :last_name, :maximum=>140 
+    
+    validates_presence_of :overall_rating
+    validates_numericality_of :overall_rating, :greater_than_or_equal_to=>0, :less_than_or_equal_to=>0
+    
+    validates_presence_of :department 
+    validates_length_of :department, :is=>4
+
 end
