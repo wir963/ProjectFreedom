fe030d796dd3fa6cb9a2b3119bc577dbeb0a2620
Welles Robinson <wir963@gmail.com>
2012-06-10 18:45:52 -0400

    tried to fix best professor

--- app/models/course.rb
+++ app/models/course.rb
@@ -5,9 +5,8 @@ class Course < ActiveRecord::Base
     has_many :courses, :through => :course_evaluations
     
     def best_professor
-        best_prof = course_evaluations.first.professor.last_name
-        max_rating = course_evaluations.first.overall_cross_rating
-        course_evaluations.each do |c|
+        best_prof = course.professors.first
+        course.professors.each do |c|
             if c.overall_cross_rating > max_rating
                 best_prof = c.professor.last_name
                 max_rating = c.overall_cross_rating
